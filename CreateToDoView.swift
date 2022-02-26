5f048318c36d329123ad0b1413893eea3248659c
Abhinav Mathur <abhinavmathur1912@gmail.com>
2022-02-26 21:11:52 +0530

    refactor: view handling only new todo creation

--- CreateToDoView.swift
+++ CreateToDoView.swift
@@ -8,59 +8,43 @@
 import Foundation
 import SwiftUI
 
-struct DetailedTodoView: View {
-    @Environment(\.dismiss) var dismiss
-    @State var title: String = ""
-    @State var description: String = ""
-    @State var profile: Profile = .other
-    @State var deadline: Date = .now
-    @State var viewingMode: ViewingMode = .adding
+struct CreateToDoView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var title: String = ""
+    @State private var description: String = ""
+    @State private var profile: Profile = .other
+    @State private var deadline: Date = .now
     @State private var selection = Profile.work
-    @State var presentAlert: Bool = false
-    @State var cancellationAlert: Bool = false
+    @State private var presentAlert: Bool = false
+    @State private var cancellationAlert: Bool = false
     @State private var currentError: ToDoError? = nil
+
+    @State var viewingMode: ViewingMode = .adding
     weak var delegate: ToDoViewModel?
 
     var body: some View {
         NavigationView {
             VStack {
-                if self.viewingMode == .adding {
-                    TextField("Enter Title", text: $title)
-                        .font(.title)
-                        .padding(.top)
-                        .padding()
-                    TextField("Enter description (optional)", text: $description)
-                        .font(.title2)
-                        .padding()
-                    HStack {
-                        Text("Select profile:").font(.title2).padding()
-                        Menu {
-                            Picker("Select a paint color", selection: $selection) {
-                                ForEach(Profile.allCases) { cs in
-                                    Text(cs.description.capitalized)
-                                }
+                TextField("Enter Title", text: $title)
+                    .font(.title)
+                    .padding(.top)
+                    .padding()
+                TextField("Enter description (optional)", text: $description)
+                    .font(.title2)
+                    .padding()
+                HStack {
+                    Text("Select profile:").font(.title2).padding()
+                    Menu {
+                        Picker("Select a paint color", selection: $selection) {
+                            ForEach(Profile.allCases) { cs in
+                                Text(cs.description.capitalized)
                             }
-                        } label: {
-                            Text(selection.description.capitalized)
-                                .font(.title2)
-                        }.id(selection)
-                        Spacer()
-                    }
-                } else {
-                    HStack {
-                        Text("Title").font(.title)
-                            .padding(.leading)
-                            .padding(.top)
-                            .padding(.top)
-                        Spacer()
-                    }
-
-                    HStack {
-                        Text("Description").font(.title3)
-                            .padding(.leading)
-                            .padding(.top)
-                        Spacer()
-                    }
+                        }
+                    } label: {
+                        Text(selection.description.capitalized)
+                            .font(.title2)
+                    }.id(selection)
+                    Spacer()
                 }
                 Spacer()
             }
@@ -110,7 +94,11 @@ struct DetailedTodoView: View {
     }
 
     private func getToDo() -> ToDo {
-        return ToDo(taskName: self.title, description: nil, profile: self.profile, deadline: nil)
+        return ToDo(
+            taskName: self.title,
+            description: self.description.isEmpty ? nil : self.description,
+            profile: self.profile,
+            deadline: nil)
     }
 }
 
--- ToDoView.swift
+++ ToDoView.swift
@@ -49,7 +49,7 @@ struct ToDoView: View {
             }
         }
         .fullScreenCover(isPresented: $showingSheet, onDismiss: nil, content: {
-            DetailedTodoView(delegate: self.viewModel)
+            CreateToDoView(delegate: self.viewModel)
         })
     }
 }
